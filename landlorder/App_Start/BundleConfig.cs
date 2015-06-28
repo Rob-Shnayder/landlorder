fac81e48d9544237c5d6627112997afdbec261f4
Rob-Shnayder <rjs12h@my.fsu.edu>
2015-06-28 19:03:23 -0400

    Initial load

---
+++ landlorder/App_Start/BundleConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace landlorder
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+            
+
+            bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
+                "~/scripts/jquery-1.11.1.min.js",
+                "~/scripts/jquery-migrate-1.2.1.min.js",
+                "~/scripts/jquery.easing.1.3.js",
+                "~/scripts/bootstrap/js/bootstrap.min.js",
+                "~/scripts/jquery-scrollTo/jquery.scrollTo.min.js",
+                "~/scripts/main.js"));
+
+
+
+
+          
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/scripts/bootstrap/css/bootstrap.min.css",
+                      "~/Content/site.css",
+                      "~/content/font-awesome/css/font-awesome.css"));
+        }
+    }
+}
---
+++ landlorder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="landlorder.MvcApplication" Language="C#" %>
